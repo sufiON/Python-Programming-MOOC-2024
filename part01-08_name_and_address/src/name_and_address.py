55e6ee34db9b9dbc0af56f7df6d78d23c9a2628a
Abu Sufian <abusufian@Abus-Mac-mini.local>
2024-12-12 12:47:46 +0600

    update

--- part01-08_name_and_address/src/name_and_address.py
+++ part01-08_name_and_address/src/name_and_address.py
@@ -1,1 +1,8 @@
 # Write your solution here
+p_name = input("Given name:")
+f_name = input("Family name:")
+s_name = input("Street adress:")
+c_and_postalcode = input("City and postal code")
+print(p_name, f_name)
+print(s_name)
+print(c_and_postalcode)
